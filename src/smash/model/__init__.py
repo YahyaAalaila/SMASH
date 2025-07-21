121fc0f636cbcdf26678d18088deb2c15ea523f8
Yahya Aalaila <yahya.aalaila@um6p.ma>
2025-07-21 10:40:34 +0200

    Ipdate model imports

--- src/smash/model/__init__.py
+++ src/smash/model/__init__.py
@@ -1,3 +1,14 @@
-from model.SM_model import ScoreMatch_module, SMASH, Model_all
-from  model.Models import Transformer, Transformer_ST
-from  model.Metric import get_calibration_score
+from __future__ import annotations
+
+from .SM_model import ScoreMatch_module, SMASH, Model_all
+from  .Models import Transformer, Transformer_ST
+from  .Metric import get_calibration_score
+
+__all__ = [
+    "ScoreMatch_module",
+    "SMASH",
+    "Model_all",
+    "Transformer",
+    "Transformer_ST",
+    "get_calibration_score",
+]
